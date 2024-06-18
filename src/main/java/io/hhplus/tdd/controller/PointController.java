4d057a40330e14e96cd0fddfe62964dbae8952c9
[헬스케어]박시현A <si0852@douzone.com>
2024-06-18 17:12:07 +0900

    feat: si0852 특정 유저의 포인트를 사용하는 기능 Controller Test: id 넣었을때 200 코드 리턴

--- src/main/java/io/hhplus/tdd/controller/PointController.java
+++ src/main/java/io/hhplus/tdd/controller/PointController.java
@@ -58,10 +58,10 @@ public class PointController {
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
-    @PatchMapping("{id}/use")
+    @PatchMapping("{userId}/use")
     public UserPoint use(
-            @PathVariable long id,
-            @RequestBody long amount
+            @PathVariable(name = "userId") long userId,
+            @RequestBody long point
     ) {
         return new UserPoint(0, 0, 0);
     }
--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,8 +1,11 @@
 package io.hhplus.tdd.point;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.hhplus.tdd.service.PointServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +26,7 @@ import static org.springframework.web.servlet.function.RequestPredicates.content
 @SpringBootTest
 class PointControllerTest {
 
+    private static final Logger log = LoggerFactory.getLogger(PointControllerTest.class);
     @Autowired
     MockMvc mvc;
 
@@ -62,13 +66,23 @@ class PointControllerTest {
     }
 
     @Test
-    @DisplayName("특정 유저의 포인트를 충전하는 Controller 두번째 테스트: 서비스 로직 추가")
-    void charge_add_userpoint() throws Exception {
-        //given
-        //when
-        //then
-    }
+    @DisplayName("특정 유저의 포인트를 사용하는 기능 Controller Test: id 넣을을때 200 코드 리턴")
+    void use() throws Exception{
+            //given
+            long userId = 1L;
+            long point = 1000L;
+            UserPoint userPoint = new UserPoint(0, 0, 0);
+            String content = objectMapper.writeValueAsString(userPoint);
 
+            //when
+            //then
+            mvc.perform(patch("/point/" + userId + "/use")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(String.valueOf(point)))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json(content));
+
+    }
 
 //    @Test
 //    void history() {
